af9ce74c309ef6c07009d65871ea3f7c7bf2cf67
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-18 23:25:42 -0300

    Este programa pergunta ao usuário a idade de um artista famoso

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace idade_de_um_artista_famoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos anos tem o artista Carlos Alberto de Nóbrega?");
+            int resposta = int.Parse(Console.ReadLine());
+            while(resposta!=84){
+                Console.WriteLine("Tente novamente!");
+                Console.WriteLine("Quantos anos tem o artista Carlos Alberto de Nóbrega?");
+                resposta = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Parabéns, você acertou!");
+            
+        }
+    }
+}
